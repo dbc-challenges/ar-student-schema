f0ca3a5655f7a2dd14947a4f256796080b2b9dcc
Apprentice <apprentice@dbc28.local>
2013-02-14 16:06:29 -0800

    implemented change method

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -4,6 +4,16 @@ require_relative '../config'
 
 class CreateStudents < ActiveRecord::Migration
   def change
-    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |t|
+      t.integer   :id
+      t.string    :first_name
+      t.string    :last_name
+      t.string    :gender
+      t.string    :email
+      t.string    :phone
+      t.date      :birthday
+      # t.datetime  :created_at
+      # t.datetime  :updated_at 
+    end
   end
 end
