ae3e8fc3f856ac8bfa548a56f7ee964da7cdaf55
Apprentice <apprentice@dbc09.local>
2013-03-07 14:37:26 -0800

    Pass all tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,10 +1,17 @@
 require_relative '../../db/config'
 
-class Student
+class Student < ActiveRecord::Base
+  validates :email, :uniqueness => true
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  validates :phone, :format => { :with => /^(1\s*[-\/\.]?)?(\((\d{3})\)|(\d{3}))\s*[-\/\.]?\s*(\d{3})\s*[-\/\.]?\s*(\d{4})\s*(([xX]|[eE][xX][tT])\.?\s*(\d+))*$/}
+  validates :age, :numericality => { :greater_than => 4 }
+
   def name
+    "#{first_name} #{last_name}"
   end
 
   def age
+    (DateTime.now - birthday).to_i / 365
   end
-  
+
 end
